8274011a2b99cabd337fc62a820f7422a9ad3733
TobiasSchuschnig <tschuschnig@student.tgm.ac.at>
2014-02-04 19:35:16 +0100

    Analytics Server
    Analytics Server more implementations

--- src/analyticserver/AnalyticServer.java
+++ src/analyticserver/AnalyticServer.java
@@ -1,5 +1,49 @@
 package analyticserver;
 
-public class AnalyticServer extends Remote{
+import java.util.concurrent.ConcurrentHashMap;
+
+import model.*;
+
+public class AnalyticServer implements AnalyticServerInterface{
+	
+	private ConcurrentHashMap<Integer,AuctionEvent> auctionEvents;
+	private ConcurrentHashMap<Integer,UserEvent> userEvents;
+	private ConcurrentHashMap<Integer,BidEvent> bidEvents;
+	private ConcurrentHashMap<Integer,StatisticsEvent> statisticsEvent;
+	private ConcurrentHashMap<String,String> managementClients; //incorect 
+	//Important: first String is an ID for the Management Client
+	// 	private ConcurrentHashMap<String,String> managementClients; //correct but not implemented now
+
+	@Override
+	public String suscribe(String filter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void processEvent(Event event) {
+		// TODO Auto-generated method stub
+		if(event instanceof AuctionEvent) {
+			auctionEvents.put(event.getID(), event);
+			this.claculate();
+		}
+		else if(event instanceof AuctionEvent) {
+			
+		}else if(event instanceof AuctionEvent) {
+			
+		}else if(event instanceof AuctionEvent) {
+			
+		}
+	}
+
+	@Override
+	public void unsuscribe(String uid) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void claculate() {
+		
+	}
 	
 }
--- src/analyticserver/AnalyticServerInterface.java
+++ src/analyticserver/AnalyticServerInterface.java
@@ -3,6 +3,8 @@ package analyticserver;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+import model.Event;
+
 public interface AnalyticServerInterface extends Remote {
 	public String suscribe(String filter);
 	public void processEvent(Event event);
