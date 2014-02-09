2f3051c180595194e48b1647b78aaa374e6ea546
AlexanderAuradnik <alexander.auradnik@student.tgm.ac.at>
2014-02-09 16:45:53 +0100

    added blank analyticsserver test

---
+++ src/analyticserver/junit/AnalyticsServerInterfaceTest.java
@@ -0,0 +1,14 @@
+package analyticserver.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AnalyticsServerInterfaceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
