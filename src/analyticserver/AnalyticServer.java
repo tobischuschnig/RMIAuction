81ce6d14f07b03b117822450f026460e144b51cd
TobiasSchuschnig <tschuschnig@student.tgm.ac.at>
2014-02-10 08:36:12 +0100

    Continued the Work on Analytik Server

--- src/analyticserver/AnalyticServer.java
+++ src/analyticserver/AnalyticServer.java
@@ -1,5 +1,7 @@
 package analyticserver;
 
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.concurrent.ConcurrentHashMap;
 
 import model.*;
@@ -24,7 +26,7 @@ public class AnalyticServer implements AnalyticServerInterface{
 		statisticsEvents.put(EventType.USER_SESSIONTIME_AVG,new StatisticsEvent());
 		statisticsEvents.put(EventType.BID_PRICE_MAX,new StatisticsEvent());
 		statisticsEvents.put(EventType.BID_COUNT_PER_MINUTE,new StatisticsEvent());
-		statisticsEvents.put(EventType.AUCTION_TYPE_AVG,new StatisticsEvent());
+		statisticsEvents.put(EventType.AUCTION_TIME_AVG,new StatisticsEvent());
 		statisticsEvents.put(EventType.ACUTION_SUCCESS_RATIO,new StatisticsEvent());		
 	}
 	
@@ -38,12 +40,20 @@ public class AnalyticServer implements AnalyticServerInterface{
 	public void processEvent(Event event) {
 		// TODO Auto-generated method stub
 		if(event instanceof AuctionEvent) {
-			auctionEvents.put(auctionEvents.size(), (AuctionEvent) event);
-			this.claculateAuctionStatistic();
+			if (event.getType().equals(EventType.AUCTION_STARTED)) {
+				auctionEvents.put((int) ((AuctionEvent) event).getAuctionID(), (AuctionEvent) event);
+			} else {
+				auctionEvents.put((int) -((AuctionEvent) event).getAuctionID(), (AuctionEvent) event);
+
+			}
+			
+			this.claculateAuctionStatistic((int) ((AuctionEvent) event).getAuctionID());
 		}
 		else if(event instanceof UserEvent) {
 			userEvents.put(userEvents.size(), (UserEvent) event);
 			this.claculateUserStatistic();
+		
+		
 		}else if(event instanceof BidEvent) {
 			bidEvents.put(bidEvents.size(), (BidEvent) event);
 			this.claculateBidStatistic();
@@ -61,9 +71,14 @@ public class AnalyticServer implements AnalyticServerInterface{
 	}
 	
 
-	private void claculateAuctionStatistic() {
-		// TODO Auto-generated method stub
-		//auctionEvents.
+	private void claculateAuctionStatistic(Integer newkey) {
+		Enumeration<Integer> keys = auctionEvents.keys();
+		while(keys.hasMoreElements()) {
+			int current = keys.nextElement();
+			auctionEvents.get(keys.nextElement());
+		}
+		Double past = statisticsEvents.get(EventType.AUCTION_TIME_AVG).getValue();
+		past *= auctionEvents.size()/2;
 	}
 	
 	private void claculateUserStatistic() {
--- src/analyticserver/Spielwiese.java
+++ src/analyticserver/Spielwiese.java
@@ -1,13 +1,30 @@
 package analyticserver;
 
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Spielwiese {
 	public static void main(String[] args) {
 		ConcurrentHashMap<Integer,String> wert = new ConcurrentHashMap();
+		//HashMap<Integer,String> wert = new HashMap<Integer,String>();
 		wert.put(1, "Hallo");
-		wert.put(1, "Hallo2");
-		String wert1 = wert.get(1);
+		wert.put(-1, "Hallo2");
+		int i = 1;
+		String wert1 = wert.get(i);
+		String wert2 = wert.get(-i);
 		System.out.println(wert1);
+		System.out.println(wert2);
+		
+		System.out.println("");
+		
+		Enumeration<Integer> wert3 = wert.keys();
+		while(wert3.hasMoreElements()) {
+			System.out.println(wert3.nextElement());
+			
+		}
+		
+		System.out.println(wert.size());
 	}
 }
