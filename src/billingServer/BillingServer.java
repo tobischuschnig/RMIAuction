e994315d9b96efbf614bcee616224359402fe22d
TobiasLins <tlins@student.tgm.ac.at>
2014-02-11 13:49:35 +0100

    md5 password generation fixed
    userlogin now working with md5

--- src/billingServer/BillingServer.java
+++ src/billingServer/BillingServer.java
@@ -12,6 +12,8 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.security.MessageDigest;
+
+import Exceptions.UserInputException;
 /**
  * This is the class for the user login.
  * If the login is successfully, you will get
@@ -24,11 +26,11 @@ import java.security.MessageDigest;
  *
  */
 public class BillingServer implements BillingServerInterface,Serializable {
-	
-	
-	
+
+
+
 	private static Registry r;
-	
+
 	/**
 	 * The access to the billing server is secured by user authentication. To keep things simple, 
 	 * the username/password combinations can be configured statically in a config file user.properties. 
@@ -42,20 +44,22 @@ public class BillingServer implements BillingServerInterface,Serializable {
 	 * @param password	password from this user
 	 * @return	reference von BillingServerSecure
 	 */
-	public BillingServerSecureInterface login(String username, String password) throws RemoteException{
+	public BillingServerSecureInterface login(String username, String password) throws RemoteException,UserInputException{
 		FileInputStream fis = null;
 		BufferedReader br = null;
 		boolean login = false;
-		
+
 		try {
 			fis = new FileInputStream("user.properties");
 			br = new BufferedReader(new InputStreamReader(fis));
 			String line = br.readLine();
 			while(line != null){
 				String[] parts = line.split("=");
-				if(parts[0].equals(username) && parts[1].equals(createMD5(password)))
+				if(parts[0].equals(username) && parts[1].equals(createMD5(password))){
 					login = true;
+				}
 				line = br.readLine();
+
 			}
 			fis.close();
 			br.close();
@@ -65,59 +69,62 @@ public class BillingServer implements BillingServerInterface,Serializable {
 			System.err.println("Error loading file : user.properties");
 		}
 		//Rueckgabe der referenz
-		if(login)
+		if(login){
 			try {
 				return (BillingServerSecureInterface)r.lookup("BillingServerSecure");
 			} catch (NotBoundException e) {
 				System.out.println("Service not bound");
 			}
-		try {
-			//throw new invalidinputexception
-			//throw new RemoteException();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			//e.printStackTrace();
+		}else{
+			throw new UserInputException();	
 		}
-		//return null;
 		return null;
 	}
-	
+
+	/**
+	 * Creates the md5 hash from a string
+	 * @param pass password to be hashed
+	 * @return md5 hash from password
+	 */
 	public String createMD5(String pass){
 		try{
-			MessageDigest md5 = MessageDigest.getInstance("MD5");
-			md5.reset();
-	        md5.update(pass.getBytes());
-	        byte[] result = md5.digest();
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(pass.getBytes());
 
-	        /* Ausgabe */
-	        StringBuffer hexString = new StringBuffer();
-	        for (int i=0; i<result.length; i++) {
-	            hexString.append(Integer.toHexString(0xFF & result[i]));
-	        }
-	        return hexString.toString();
+			byte byteData[] = md.digest();
+
+			//convert the byte to hex format method 1
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < byteData.length; i++) {
+				sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+			}
+
+			// System.out.println("Digest(in hex format):: " + sb.toString());
+
+			return sb.toString();
 		}catch (Exception e) {
 			return "";
 		}
 	}
 
 	public static void main(String args[]){
-	    // Ein SecurityManager ist nur erforderlich, wenn der RMI-Class-Loader Code laden muss
-//	    if (System.getSecurityManager() == null)
-//	      System.setSecurityManager(new RMISecurityManager());
-	    try {
-	    	r=LocateRegistry.createRegistry(1099);
-	    	//LocateRegistry.createRegistry(1099);
-	    	BillingServer acc = new BillingServer();
-	    	BillingServerSecure secure=new BillingServerSecure();
-	    	r.rebind("BillingServerSecure", (BillingServerSecureInterface)UnicastRemoteObject.exportObject(secure, 0));
-	    	System.out.println("BillingServerSecure bound");
-	    	r.rebind("BillingServer", (BillingServerInterface)UnicastRemoteObject.exportObject(acc, 0));
-	    	System.out.println("BillingServer bound");
-	    } 
-	    catch (Exception e) {
-	      System.out.println("Err: " + e.getMessage());
-	    }
-	  }
+		// Ein SecurityManager ist nur erforderlich, wenn der RMI-Class-Loader Code laden muss
+		//	    if (System.getSecurityManager() == null)
+		//	      System.setSecurityManager(new RMISecurityManager());
+		try {
+			r=LocateRegistry.createRegistry(1099);
+			//LocateRegistry.createRegistry(1099);
+			BillingServer acc = new BillingServer();
+			BillingServerSecure secure=new BillingServerSecure();
+			r.rebind("BillingServerSecure", (BillingServerSecureInterface)UnicastRemoteObject.exportObject(secure, 0));
+			System.out.println("BillingServerSecure bound");
+			r.rebind("BillingServer", (BillingServerInterface)UnicastRemoteObject.exportObject(acc, 0));
+			System.out.println("BillingServer bound");
+		} 
+		catch (Exception e) {
+			System.out.println("Err: " + e.getMessage());
+		}
+	}
 
 
 }
--- src/billingServer/BillingServerInterface.java
+++ src/billingServer/BillingServerInterface.java
@@ -3,6 +3,8 @@ package billingServer;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+import Exceptions.UserInputException;
+
 public interface BillingServerInterface extends Remote {
-		  public BillingServerSecureInterface login(String username, String password) throws RemoteException;
+		  public BillingServerSecureInterface login(String username, String password) throws RemoteException,UserInputException;
 }
