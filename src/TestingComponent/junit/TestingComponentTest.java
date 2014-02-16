42816ffbfc5f5a47cdda595843985a3f67d24a9e
AlexanderRieppel <arieppel@student.tgm.ac.at>
2014-02-16 18:59:40 +0100

    edited and added some testcases

---
+++ src/TestingComponent/junit/TestingComponentTest.java
@@ -0,0 +1,29 @@
+package TestingComponent.junit;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import TestingComponent.TestingComponent;
+
+/**
+ * Testclass for TestingComponent
+ * @author Alexander Rieppel
+ * @email <arieppel@student.tgm.ac.at>
+ */
+public class TestingComponentTest {
+	
+	
+	@Before
+	public void setUp(){
+		
+	}
+	
+	/**
+	 * Testing of auto Method, when autoprint should equals true
+	 */
+	@Test
+	public void testAuto(){
+
+	}
+}
+
