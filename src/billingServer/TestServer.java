e002750844397bf62037b2b7773cf7c4ebb8d9fc
TobiasLins <tlins@student.tgm.ac.at>
2014-02-19 12:59:12 +0100

    new testcase fix PLEASE KLUNEE"!!!!§"!§ LOL
    if i run test 2 times, i cant add new pricestep
    . wow
    so much bug
    very win please fixed

--- src/billingServer/TestServer.java
+++ src/billingServer/TestServer.java
@@ -10,7 +10,7 @@ import model.PriceStep;
 
 public class TestServer {
 	public static void main (String [] args){
-		
+
 		//BillingServer Objekt wird erstellt, und durch Naming Lookup abgerufen
 		BillingServerInterface acc = null;
 		try {      
@@ -19,25 +19,34 @@ public class TestServer {
 		catch (Exception e) {
 			e.printStackTrace();
 		}
+		BillingServerSecureInterface secure = null;
 		try {
 			// BillingServerSecure objekt wird erstellt. 
 			//Login hat zwei parameter (username, password) aber unverschluesselt.
-			BillingServerSecureInterface secure=(BillingServerSecureInterface)acc.login("admin", "admin");
+			secure=(BillingServerSecureInterface)acc.login("admin", "admin");
+			secure.createPriceStep(21.0, 31.0, 2.0, 5.0);
 			secure.createPriceStep(1.0, 5.0, 2.0, 5.0);
 			secure.createPriceStep(7.0, 10.0, 2.0, 5.0);
+			secure.createPriceStep(11.0, 20.0, 2.0, 5.0);
+			//secure.billAuction("admin", 1, 1.2);
 
-			secure.billAuction("admin", 1, 1.2);
-			System.out.println(secure.getPriceSteps().toString());
-			
-			secure.deletePriceStep(1.0, 5.0);
-			System.out.println(secure.getPriceSteps().toString());
-			
-			Bill b=secure.getBill("admin");
-		//	b.
+			//	b.
 		} catch (RemoteException e) {
 		}catch (UserInputException e1) {
 			System.err.println(e1.getMessage());
 		}
+		try {
+			System.out.println(secure.getPriceSteps().toString());
+			secure.deletePriceStep(1.0, 5.0);
+			System.out.println(secure.getPriceSteps().toString());
+
+			Bill b=secure.getBill("admin");
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
 	}
 
 }
