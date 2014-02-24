dd8be1d33d3b429c60e7b6de20bb76ef64747996
AlexanderAuradnik <alexander.auradnik@student.tgm.ac.at>
2014-02-24 09:39:44 +0100

    alltest format

--- src/alltest/AllTest.java
+++ src/alltest/AllTest.java
@@ -10,14 +10,27 @@ import org.junit.runners.Suite.SuiteClasses;
 /**
  * Overall Testsuite for all Testcases which are written so far
  * @author Alexander Rieppel <alexander.rieppel@student.tgm.ac.at>
+ * @author Alexander Auradnik <alexander.auradnik@student.tgm.ac.at>
  * @version 13-01-2013
  */
 
 @RunWith(Suite.class)
-@SuiteClasses({ server.test.AuctionHandlerTest.class, server.test.ServerTest.class, analyticserver.junit.AnalyticsServerTest.class, analyticserver.junit.EventHandlerTest.class,
-	billingServer.junit.BillingServerTest.class,billingServer.junit.BillingServerSecureTest.class,model.test.AuctionTest.class,
-	model.test.UserTest.class,model.test.AllModel.class,managmentclient.junit.ManagementCLientInputTest.class,
-	managmentclient.junit.TaskExecuterTest.class,managmentclient.junit.ManagementClientTest.class})
+@SuiteClasses({ 
+	
+	server.test.AuctionHandlerTest.class,
+	server.test.ServerTest.class,
+	analyticserver.junit.AnalyticsServerTest.class,
+	analyticserver.junit.EventHandlerTest.class,
+	analyticserver.junit.AllAnalyticsServer.class,
+	billingServer.junit.BillingServerTest.class,
+	billingServer.junit.BillingServerSecureTest.class,
+	model.test.AuctionTest.class,
+	model.test.UserTest.class,
+	model.test.AllModel.class,
+	managmentclient.junit.ManagementCLientInputTest.class,
+	managmentclient.junit.TaskExecuterTest.class,
+	managmentclient.junit.ManagementClientTest.class})
+
 public class AllTest {
 	@BeforeClass 
 	public static void setUpOnce() {
