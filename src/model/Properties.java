da2875944c411b69f3b6c18833fc4c6f7ed5f0ab
AlexanderDominikMarioReinhardKlune <aklune@student.tgm.ac.at>
2014-02-25 09:58:41 +0100

    doc properties.java

--- src/model/Properties.java
+++ src/model/Properties.java
@@ -3,14 +3,25 @@ package model;
 import java.io.IOException;
 import java.net.URL;
 
+/**
+ * This Class gets a Path as parameter. this path directs to the specific 
+ * properties file to check login data or something else
+ * @author Tobias Lins
+ * @email tlins@student.tgm.ac.at
+ * @version 1.0
+ */
 public class Properties {
 	String file;
+	/**
+	 * Sets the Path
+	 * @param file	Path to the properties file
+	 */
 	public Properties(String file){
 		this.file=file;
 	}
 	/**
 	 * Get the property value by name
-	 * @param name
+	 * @param name	value of the key
 	 */
 	public String getProperty(String name) {
 		java.io.InputStream is = ClassLoader.getSystemResourceAsStream(this.file);
