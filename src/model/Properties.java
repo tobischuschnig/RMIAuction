dc4a45041dddaa327d1946253aac8552b5f0bf34
TobiasLins <tlins@student.tgm.ac.at>
2014-02-25 08:47:56 +0100

    property model added use it to get properties from the file!

---
+++ src/model/Properties.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.io.IOException;
+
+public class Properties {
+	String file;
+	public Properties(String file){
+		this.file=file;
+	}
+	/**
+	 * Get the property value by name
+	 * @param name
+	 */
+	public String getProperty(String name) {
+		java.io.InputStream is = ClassLoader.getSystemResourceAsStream("registry.properties");
+		if (is != null) {
+			java.util.Properties props = new java.util.Properties();
+			try {
+				props.load(is);
+				return props.getProperty(name);
+			} catch (IOException e) {
+				System.out.println("cant read file "+file);
+				
+			} finally {
+				try {
+					is.close();
+				} catch (IOException e) {	
+				}
+			}
+		} else {
+			System.err.println("Properties file not found!");
+		}
+		return "";
+	}
+}
